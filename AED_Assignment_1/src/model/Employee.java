c686cdcf9e4a60289d1a938d5c2e8ac13b62685a
Mohitha Edara <edara.m@northeastern.edu>
2022-10-01 20:42:13 -0400

    2nd Commit

---
+++ AED_Assignment_1/src/model/Employee.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import java.util.*;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+
+
+
+/**
+ *
+ * @author mohitha
+ */
+public class Employee {
+    private String name;
+    private long empId;
+    private int age;
+    private String gender;
+    private Date startDate;
+    private String level;
+    private String teamInfo;
+    private String posTitle;
+    private long phoneNum;
+    private String emailId;
+    private Image photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(long empId) {
+        this.empId = empId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getPosTitle() {
+        return posTitle;
+    }
+
+    public void setPosTitle(String posTitle) {
+        this.posTitle = posTitle;
+    }
+
+    public long getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(long phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public Image getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Image photo) {
+        this.photo = photo;
+    }
+    
+    
+    
+    
+    
+    
+}
