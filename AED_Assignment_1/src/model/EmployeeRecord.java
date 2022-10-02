8450415d9f9dfe72f610ad33fc9716996aff9eb0
Mohitha Edara <edara.m@northeastern.edu>
2022-10-01 20:59:27 -0400

    3rd Commit

---
+++ AED_Assignment_1/src/model/EmployeeRecord.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mohitha
+ */
+public class EmployeeRecord {
+    private ArrayList<Employee> record;
+    
+    
+    
+    
+}
